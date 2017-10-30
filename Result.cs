069ad30c8f6daa3e04471faddc69934dc5112003
nnersoft <10000@365nner.com>
2017-10-30 10:15:32 +0800

    NnerSoft

---
+++ Result.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NnerSoft.Bas.Model
+{
+    /// <summary>
+    /// 返回结果
+    /// </summary>
+    public class Result
+    {
+    }
+}
